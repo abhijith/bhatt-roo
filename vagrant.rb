2f9ab504c7c11ba0fe4d63151fceeb4b9ad284db
Abhijith <abhijithg@gmail.com>
2015-06-03 16:11:32 +0530

    working versions without pinning

--- vagrant.rb
+++ vagrant.rb
@@ -3,10 +3,10 @@ require 'chef/provisioning/vagrant_driver'
 
 vagrant_cluster "#{ENV['HOME']}/bhatt-roo-vms"
 
-vagrant_box 'base-ubuntu-1404' do
-  url "https://oss-binaries.phusionpassenger.com/vagrant/boxes/latest/ubuntu-14.04-amd64-vbox.box"
-end
+# vagrant_box 'base-ubuntu-1404' do
+#   url "https://oss-binaries.phusionpassenger.com/vagrant/boxes/latest/ubuntu-14.04-amd64-vbox.box"
+# end
 
 with_machine_options :vagrant_options => {
-  'vm.box' => 'base-ubuntu-1404'
+  'vm.box' => 'stockvm'
 }
